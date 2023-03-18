8bfe1bd913e8ba081da5c970da4227fbefa352e2
kostya <exmpl@gmail.com>
2023-03-18 02:10:03 +0200

    objects_practice

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 /*jshint esversion: 9 */
 
-let condition = true;
-let k = 0;
 
-while (condition) {
-    const question = prompt("Say something");
-    if (question == "") {
-        const question = prompt("Say something");
-        continue;
-    } else {
-        condition = false;
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: true,
+    start: () => {
+        personalMovieDB.count = +prompt("How many movies did you watch?");
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null ||
+        isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("How many movies did you watch?");
+        }
+    },
+    showMyDB: () => {
+        if(!personalMovieDB.privat) {
+            console.log("Object is privat");
+        } else {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: (showMyDB) => {
+        if (personalMovieDB.privat) {
+            privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+        personalMovieDB.showMyDB();
+    },
+    writeYourGenres: () => {
+        for (let i = 0; i <= 2; i++) {
+            let genre = prompt("Paste");
+        }
+    personalMovieDB.genres.forEach((a, i) => {
+        console.log(`Favourite genre #${i+1} is ${a}`);
+    });
     }
-}
+};
+
+
+personalMovieDB.writeYourGenres();
+
+
+
 
 
